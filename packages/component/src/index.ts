45779f0aee1dea45e44ffe2191fafdcc6e360828
Andre Schmidt <vndredev@icloud.com>
2026-06-12 08:35:24 +0200

    feat: structure the component setup model for a React adapter

--- packages/component/src/index.ts
+++ packages/component/src/index.ts
@@ -2,5 +2,6 @@ export type * from "./model.ts";
 export * from "./naming.ts";
 export * from "./render-react-sfc.ts";
 export * from "./render-react.ts";
+export * from "./render-setup.ts";
 export * from "./render-vue.ts";
 export * from "./validate-name.ts";
